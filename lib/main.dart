083083b623a82dfa0dd640bb674933ca3af2ac59
Dawid Klimowicz <dawidkl777@gmail.com>
2023-02-28 21:19:27 +0100

    quiz app start

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:quizapp/pages/game_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Frivvia',
+      theme: ThemeData(
+        fontFamily: 'ArchitectsDaughter',
+        scaffoldBackgroundColor: const Color.fromRGBO(31, 31, 31, 1.0),
+        primarySwatch: Colors.blue,
+      ),
+      home: GamePage(),
+    );
+  }
+}
